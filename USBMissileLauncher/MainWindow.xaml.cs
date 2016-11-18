aa0c53e4f441650b8425166af1c9d7b785d749e5
kolyandex <iam@kolyandex.su>
2016-11-18 16:12:30 +0700

    Initial

---
+++ USBMissileLauncher/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace USBMissileLauncher
+{
+    public partial class MainWindow
+    {
+        private readonly MissileController _missileController = new MissileController(0x0A81, 0x0701);
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (_missileController.Exist) return;
+            MessageBox.Show("Device is not connected or VID/PID not match 0x0A81/0x0701");
+            Environment.Exit(-1);
+        }
+
+
+        private void FireButton_Click(object sender, RoutedEventArgs e)
+        {
+            _missileController.Fire();
+        }
+
+       
+
+        private void UpButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _missileController.Up();
+        }
+
+        private void LeftButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _missileController.Left();
+        }
+
+        private void RightButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _missileController.Right();
+        }
+
+        private void DownButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _missileController.Down();
+        }
+
+        private void All_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _missileController.Stop();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    _missileController.Up();
+                    break;
+                case Key.Down:
+                    _missileController.Down();
+                    break;
+                case Key.Left:
+                    _missileController.Left();
+                    break;
+                case Key.Right:
+                    _missileController.Right();
+                    break;
+                case Key.Space:
+                    _missileController.Fire();
+                    break;
+            }
+        }
+
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) return;
+            _missileController.Stop();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _missileController.Stop();
+        }
+    }
+}
---
+++ USBMissileLauncher/MissileController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Threading;
+using UsbLibrary;
+
+namespace USBMissileLauncher
+{
+    internal class MissileController
+    {
+        private struct Command
+        {
+            public static readonly byte[] Stop = { 0, 0x20 };
+            public static readonly byte[] Up = { 0, 0x02 };
+            public static readonly byte[] Down = { 0, 0x01 };
+            public static readonly byte[] Left = { 0, 0x04 };
+            public static readonly byte[] Right = { 0, 0x08 };
+            public static readonly byte[] Fire = { 0, 0x10 };
+            public static readonly byte[] RequestStatus = { 0, 0x40 };
+        }
+        public struct TurretState
+        {
+            public const byte Up = 0x02;
+            public const byte Down = 0x01;
+            public const byte Left = 0x04;
+            public const byte Right = 0x08;
+        }
+
+        private readonly SpecifiedDevice _missileLauncherDevice;
+        private readonly DispatcherTimer _fireTimer = new DispatcherTimer();
+        private byte _state;
+        public bool Exist;
+        /// <summary>
+        /// Обычный конструктор
+        /// </summary>
+        /// <param name="vendorId">Vendor ID</param>
+        /// <param name="productId">Product ID</param>
+        public MissileController(int vendorId, int productId)
+        {
+            _fireTimer.Tick += FireTimerTick;
+            _fireTimer.Interval = new TimeSpan(0, 0, 5); // 5 secs to shot
+            _fireTimer.IsEnabled = true;
+            _fireTimer.Stop();
+            _missileLauncherDevice = SpecifiedDevice.FindSpecifiedDevice(vendorId, productId);
+            _missileLauncherDevice.DataRecieved += DataReceived;
+            Exist = _missileLauncherDevice != null;
+        }
+        
+        private void FireTimerTick(object sender, EventArgs e)
+        {
+            Stop();
+            _fireTimer.Stop();
+        }
+
+        /// <summary>
+        /// Запрашивает состояние турели в данный момент. Подробности в структуре TurretState
+        /// </summary>
+        /// <returns>Байт состояния</returns>
+        public byte GetState()
+        {
+            RequestStatus();
+            return _state;
+        }
+
+        private void DataReceived(object sender, DataRecievedEventArgs args)
+        {
+            _state = args.data[1];
+        }
+
+        private void RequestStatus()
+        {
+            _missileLauncherDevice.SendData(Command.RequestStatus);
+        }
+
+        public void Left()
+        {
+            _missileLauncherDevice.SendData(Command.Left);
+        }
+        public void Right()
+        {
+            _missileLauncherDevice.SendData(Command.Right);
+        }
+        public void Up()
+        {
+            _missileLauncherDevice.SendData(Command.Up);
+        }
+        public void Down()
+        {
+            _missileLauncherDevice.SendData(Command.Down);
+        }
+        public void Fire()
+        {
+            _missileLauncherDevice.SendData(Command.Fire);
+            _fireTimer.Start();
+        }
+        /// <summary>
+        /// Останавливает любое действие
+        /// </summary>
+        public void Stop()
+        {
+            _missileLauncherDevice.SendData(Command.Left); // Костыль для остановки стрельбы
+            _missileLauncherDevice.SendData(Command.Stop);
+        }
+    }
+}
